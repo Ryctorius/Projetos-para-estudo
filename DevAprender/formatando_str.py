82657822abc5f7d6f4fafa35c129b79dbf75b9b0
FELIPE DABES GUIMARAES <felipedabes@gmail.com>
2022-11-17 15:55:58 -0300

    Testando_commit_e_string

---
+++ DevAprender/formatando_str.py
@@ -0,0 +1,4 @@
+aluno = 'Felipe'
+nome_do_curso = 'Fotografia'
+mensagem_de_introdução = f'Bem vindo ao curso de {nome_do_curso}, {aluno}'
+print(mensagem_de_introdução)
---
+++ DevAprender/string.py
@@ -0,0 +1,9 @@
+nome_do_instrumento = 'Guitarra Elétrica'
+nome_do_instrumento = "Guitarra Elétrica"
+print(nome_do_instrumento)
+resenha_do_filme = "O filme foi \"demais\""
+resenha_do_filme2 = "O filme foi 'demais'"
+resenha_do_filme3 = "O filme foi \ndemais"
+print(resenha_do_filme)
+print(resenha_do_filme2)
+print(resenha_do_filme3)
